7a6c965631924ceddb1197701f7942a0e041ad1c
Saiful Islam <saiful islam>
2024-05-18 00:00:06 +0600

    added all setup

---
+++ src/Router/Router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage';
+import Home from '../Pages/Home/Home';
+
+const Router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        errorElement:<ErrorPage></ErrorPage>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
+    }
+])
+
+export default Router;
